c0087448e622e76954472040458fc3d677d79de8
aasif1297 <mairajuddinkhoso92@gmail.com>
2020-08-24 02:30:31 +0500

    Test commit

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -11,6 +11,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //
       backgroundColor: Color(0xFFFFFFFF),
       body: _mainWidget(),
     );
